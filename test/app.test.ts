4c98c501116c5ee07391907ffacf0321c87b9ec2
Kim Hyunggyu <k7120792@gmail.com>
2019-07-26 10:25:43 +0900

    Modify app test for root path test

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,3 +1,13 @@
-test('sample', () => {
-  expect(true).toBe(true);
+import app from '../src/app';
+import supertest from 'supertest';
+
+describe('test the root path', () => {
+  test('it should response status 200 to the GET method', done => {
+    supertest(app)
+      .get('/')
+      .then(res => {
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
 });
