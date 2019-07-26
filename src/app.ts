abbb6bd23d3867d7864091f06ec29a0f35056bab
Kim Hyunggyu <k7120792@gmail.com>
2019-07-26 10:25:21 +0900

    Add view engine, home controller to app

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,35 @@ import { PORT, MONGODB_URI, ENV } from './utils/config';
 import { crawl } from './utils/crawl';
 import { saveRealtimeKeyword } from './controllers/realtimeKeyword';
 import logger from './utils/logger';
+import { index } from './controllers/home';
+import path from 'path';
 
 const app = express();
 const mongodbUri = MONGODB_URI || '';
 
-mongoose.connect(mongodbUri, { useNewUrlParser: true }).then(() => {
-  logger.info('db connected');
-}).catch((err) => {
-  logger.error('db connect errored');
-  logger.error(err);
-});
+mongoose
+  .connect(mongodbUri, { useNewUrlParser: true })
+  .then(() => {
+    logger.info('db connected');
+  })
+  .catch(err => {
+    logger.error('db connect errored');
+    logger.error(err);
+  });
 
 const crawlResultSaver = async () => {
   const result = await crawl();
   saveRealtimeKeyword(result);
 };
 
-setInterval(crawlResultSaver, 1000 * 10);
+crawlResultSaver();
+setInterval(crawlResultSaver, 1000 * 10 * 60);
 
 app.set('port', PORT || 3000);
 app.set('env', ENV || 'development');
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
+
+app.get('/', index);
 
 export default app;
