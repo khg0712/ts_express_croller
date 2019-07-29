08639f608b54f774dd9aa076678f0e1853eda0e7
Kim Hyunggyu <k7120792@gmail.com>
2019-07-29 12:58:52 +0900

    Modify crawl logic

--- src/utils/crawl.ts
+++ src/utils/crawl.ts
@@ -6,10 +6,11 @@ export const crawl = async () => {
   logger.info('crawl realtime keyword');
   const response = await axios('https://www.naver.com/');
   const $ = cheerio.load(response.data);
-  const crawledRealtimeKeywords = $('.ah_roll_area.PM_CL_realtimeKeyword_rolling ul').text();
-  const realtimeKeywords = crawledRealtimeKeywords
-                            .split('\n')
-                            .filter(val => val !== '')
-                            .filter(val => isNaN(parseInt(val, 10)));
-  return realtimeKeywords;
+  const crawledRealtimeKeywords = $(
+    '.ah_roll_area.PM_CL_realtimeKeyword_rolling ul > li span.ah_k',
+  );
+  const keywords: string[] = $(crawledRealtimeKeywords).map((i, ele):string => {
+    return $(ele).text();
+  }).get();
+  return keywords;
 };
