c5a4dfa95b45255ac2565f64cf2b90c7b943290a
Kim Hyunggyu <k7120792@gmail.com>
2019-07-31 10:34:44 +0900

    Modify .env* config

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,6 @@
 import { config } from 'dotenv';
 
 config();
-let path;
-switch (process.env.NODE_ENV) {
-  case 'test':
-    path = `${__dirname}/../../.env.test`;
-    break;
-  case 'production':
-    path = `${__dirname}/../../.env.production`;
-    break;
-  default:
-    path = `${__dirname}/../../.env.development`;
-}
-
-config({ path });
 
 export const PORT = process.env.PORT;
 export const MONGODB_URI = process.env.MONGODB_URI;
