23ff1c5be3999a75339717755682f28554a0d3fc
Kim Hyunggyu <k7120792@gmail.com>
2019-08-01 16:58:36 +0900

    Modify crawl test

--- test/utils/crawl.test.ts
+++ test/utils/crawl.test.ts
@@ -1,8 +1,29 @@
-import * as crawler from '../../src/controllers/crawl';
+import { crawl } from '../../src/controllers/crawl';
+import { Request, Parse } from '../../src/types/index';
 
-jest.mock('../../src/controllers/crawl.ts');
+const failedReqest: Request = () =>
+  new Promise(resolve => {
+    resolve({
+      data: '',
+      status: 404,
+    });
+  });
+const succeededReqest: Request = () =>
+  new Promise(resolve => {
+    resolve({
+      data: '1234',
+      status: 200,
+    });
+  });
+const parse: Parse = data => {
+  return `data is ${data}`;
+};
 
-test('is crawl method called', async () => {
-  await crawler.crawl();
-  expect(crawler.crawl).toHaveBeenCalled();
+describe('crawl works well', () => {
+  it('return null when response status is not 200', async () => {
+    expect(await crawl(failedReqest, parse)).toBeNull();
+  });
+  it('return successful parsed word', async () => {
+    expect(await crawl(succeededReqest, parse)).toBe('data is 1234');
+  });
 });
